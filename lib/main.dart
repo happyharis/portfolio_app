0d6d8365da7c4b4f74208a1b2a02c811eb2b626c
Haris Samingan <muhamad_haris@hotmail.com>
2021-07-07 12:37:50 +0800

    Update to portfolio app

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LinksLandingPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+List socialLinks = [
+  {'text': 'Udemy', 'url': 'https://www.udemy.com'},
+  {'text': 'LinkedIn', 'url': 'https://www.linkedin.com'},
+  {'text': 'Facebook', 'url': 'https://www.facebook.com'},
+];
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class LinksLandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SizedBox(height: 35),
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://i.ibb.co/LnFqnFs/profilepic2.png',
+              ),
+              radius: 48,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                '@haris.samingan',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                ),
+              ),
             ),
+            SizedBox(height: 20),
+            for (final data in socialLinks)
+              ButtonLink(text: data['text'], url: data['url']),
+            Spacer(),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  'Built in Flutter',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 19,
+                  ),
+                ),
+                SizedBox(width: 8),
+                Image.network(
+                  'https://www.neappoli.com/static/media/flutterImg.94b8139a.png',
+                  width: 25,
+                  height: 25,
+                ),
+              ],
+            ),
+            SizedBox(height: 23),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ButtonLink extends StatelessWidget {
+  const ButtonLink({
+    Key? key,
+    required this.text,
+    required this.url,
+  }) : super(key: key);
+
+  final String text;
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0).add(
+        EdgeInsets.symmetric(horizontal: 10),
+      ),
+      child: SizedBox(
+        width: width,
+        child: OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            primary: Color.fromRGBO(57, 224, 155, 1),
+            padding: EdgeInsets.symmetric(vertical: 20),
+          ),
+          child: Text(text, style: TextStyle(fontSize: 16)),
+          onPressed: () => launch(url),
+        ),
+      ),
     );
   }
 }
